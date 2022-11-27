537799e22c73d7af7ced02082bd1f7a285f01cf2
ampersand-github <ampersand.dev@gmail.com>
2022-11-27 12:26:52 +0900

    commit

---
+++ frontend/prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 以下はデフォルト設定、必要に応じてコメントアウトして値を変更する
+  // printWidth: 80
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: ture,
+  // singleQuote: false,
+  // quoteProps: "as-needed",
+  // jsxSingleQuote: false,
+  // trailingComma: "es5",
+  // bracketSpacing: true,
+  // arrowParens: "always",
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // jsxBracketSameLine: false,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: "preserve",
+  // htmlWhitespaceSensitivity: "css",
+  // vueIndentScriptAndStyle: false,
+  // endOfLine: "lf",
+  // embeddedLanguageFormatting: "auto"
+};
