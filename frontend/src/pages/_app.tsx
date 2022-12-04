9781ffda6ab5a819ac5e82d55e7f567a9e87f3b5
ampersand-github <ampersand.dev@gmail.com>
2022-12-05 05:54:18 +0900

    recoil

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,59 @@ import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "../services/constraints/themes";
 import Head from "next/head";
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import BasicLayout from "src/components/layouts/basic-layout";
+import { RecoilRoot } from "recoil";
+import { NextPage } from "next";
 
 // サンプル
 // https://github.com/mui/material-ui/tree/master/examples/nextjs
+
+/**
+ * https://nextjs.org/docs/basic-features/layouts
+ */
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout =
+    Component.getLayout ??
+    ((page: ReactElement) => {
+      return (
+        <BasicLayout>
+          <Component {...page} />
+        </BasicLayout>
+      );
+    });
+
+  return (
+    <React.StrictMode>
+      <Head>
+        <title>テンプレートサイト</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          {getLayout(<Component {...pageProps} />)}
+          <CssBaseline />
+        </ThemeProvider>
+      </RecoilRoot>
+    </React.StrictMode>
+  );
+};
+
+export default MyApp;
+/*
+
+  return getLayout(<Component {...pageProps} />)
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
@@ -14,13 +62,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>テンプレートサイト</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <BasicLayout>
-          <Component {...pageProps} />
-        </BasicLayout>
-        <CssBaseline />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <BasicLayout>
+            <Component {...pageProps} />
+          </BasicLayout>
+          <CssBaseline />
+        </ThemeProvider>
+      </RecoilRoot>
     </React.StrictMode>
   );
 }
-export default MyApp;
+
+
+ */
