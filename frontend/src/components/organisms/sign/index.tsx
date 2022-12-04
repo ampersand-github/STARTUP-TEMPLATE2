aa3ae758deec6fc263cee4e2df2d93c8d48777be
ampersand-github <ampersand.dev@gmail.com>
2022-12-05 05:56:45 +0900

    sign-form

--- frontend/src/components/organisms/sign/index.tsx
+++ frontend/src/components/organisms/sign/index.tsx
@@ -1,26 +1,32 @@
+import * as React from "react";
 import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import * as React from "react";
+import { useAuth } from "src/services/hooks/use-auth";
 
 export interface ISignForm {
   text: string;
 }
 
 export const SignForm = ({ text }: ISignForm) => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  // todo containerへ
+  const { user, signIn, logout } = useAuth();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get("email"),
       password: data.get("password"),
     });
+    await signIn(data.get("email") as string, data.get("password") as string);
   };
 
   return (
     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <p>{user ? user.uid : "null"}</p>
       <TextField
         margin="normal"
         required
