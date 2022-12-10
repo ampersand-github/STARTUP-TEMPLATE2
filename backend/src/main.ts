e675f0c3ae0006dc6e7248fceeab88f35e565cbb
ampersand-github <ampersand.dev@gmail.com>
2022-12-11 06:04:38 +0900

    セキュリティ

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import helmet from '@fastify/helmet';
+import fastifyCsrf from '@fastify/csrf-protection';
 
 async function bootstrap() {
   const fastify = new FastifyAdapter();
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastify);
-  await app.listen(3000, '0.0.0.0');
+
+  // 以下セキュリティ
+  await app.register(helmet);
+  app.enableCors();
+  await app.register(fastifyCsrf);
+
+  // アプリケーション
+  await app.listen(3011, '0.0.0.0');
 }
 
 bootstrap();
