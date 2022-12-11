7c4b6eec2c72fb03f28fae89159571dc2885f80a
ampersand-github <ampersand.dev@gmail.com>
2022-12-11 18:45:41 +0900

    定数整理

---
+++ frontend/src/services/constraints/url/api-url.ts
@@ -0,0 +1,1 @@
+export const API_PERSONS = `https://umayadia-apisample.azurewebsites.net/api/persons`;
--- frontend/src/services/constraints/url/page-url.ts
+++ frontend/src/services/constraints/url/page-url.ts
@@ -5,6 +5,7 @@ export const SIGN_IN_PAGE = "/sign/sign-in";
 export const SIGN_NO_EMAIL_VERIFIED_PAGE = "/sign/no-email-verified";
 export const PRACTICE_ANOTHER_LAYOUT = "/practice/another-layout";
 export const WISE_SAYING_PAGE = "/wise-saying";
+export const API_PRACTICE_PAGE = "/practice/api-page";
 
 // 外部ページ
 export const AMPERSAND_PAGE = "https://blog.ampersand.today/";
