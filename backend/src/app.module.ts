9a0492e0967f2028b1e2ccde7cc2da4e041a9cd8
ampersand-github <ampersand.dev@gmail.com>
2022-12-11 17:20:22 +0900

    環境変数修正

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,12 @@ import { AppController } from "src/module/app/app.controller";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "src/module/auth/auth.module";
 
-const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     ConfigModule.forRoot({
       // 全てのmoduleで使用できるように
       isGlobal: true,
-      // NODE_ENVの値によって読み込むファイルを変更する
-      // todo 書き方考える
-      envFilePath: ENV === "development" ? ".env.development" : `.env.${ENV}`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     AuthModule,
   ],
