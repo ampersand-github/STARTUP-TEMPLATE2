68c3ffd55e27866c73a31bce6878f181610b0c75
ampersand-github <ampersand.dev@gmail.com>
2023-03-02 13:20:46 +0900

    不要コード削除

--- backend/src/module/auth/auth.controller.ts
+++ backend/src/module/auth/auth.controller.ts
@@ -1,16 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "src/module/auth/auth.service";
 import { IVerifiedStateResponseDto } from "src/module/auth/verified-state-response-dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get()
-  public async gry(): Promise<string> {
-    console.log("afsfsfs");
-    return "aaaa";
-  }
 
   @Post()
   public async isVerifyState(@Body() dto: { idToken: string }): Promise<IVerifiedStateResponseDto> {
