645629ed6d012c011580a773f73fd8451dd1d7fe
ampersand-github <ampersand.dev@gmail.com>
2023-04-12 13:36:47 +0900

    メンテナンスページの作成

--- frontend/src/pages/maintenance.tsx
+++ frontend/src/pages/maintenance.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { NextPage } from "next";
-import { MaintenanceTemplate } from "src/common/components/templates/maintenance-tamplate";
+import { Center } from "@common/components/elements/center";
+import { Typography } from "@mui/material";
 
 const Maintenance: NextPage = () => {
-  return <MaintenanceTemplate />;
+  return (
+    <Center space={3}>
+      <Typography variant={"h2"} fontWeight={"bold"}>
+        メンテナンス
+      </Typography>
+      <Typography variant={"body1"}>只今、メンテナンス中です。</Typography>
+    </Center>
+  );
 };
 
 export default Maintenance;
