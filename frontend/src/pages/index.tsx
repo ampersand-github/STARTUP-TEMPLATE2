cf8211e2e448333e61746a801d6ab376703fadf0
ampersand-github <ampersand.dev@gmail.com>
2023-04-12 13:35:49 +0900

    インデックスページの更新

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,22 @@
-import { HomeTemplate } from "@common/components/templates/home-template";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { NextSeo } from "next-seo";
 
 const Home: NextPage = () => {
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   return (
     <Stack>
       <NextSeo />
-      <HomeTemplate />
+      <>
+        {text} {text} {text} {text} {text} {text} {text} {text} {text} {text}
+        {text} {text} {text} {text} {text} {text} {text} {text} {text} {text}
+      </>
     </Stack>
   );
 };
